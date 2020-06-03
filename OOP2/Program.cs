a0e6e71b35b5e0d0d2860eaa8729e19823b6ec64
NarTi_000 <NarTiiiNDK@mail.ru>
2020-06-04 00:51:57 +0500

    Добавьте файлы проекта.

---
+++ OOP2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+
+    class TV
+    {
+        int correctChannel = 0;
+        bool TVEnabled;
+        int TVChannel;
+        public void TVOn()
+        {
+            TVEnabled = true;
+            Console.WriteLine("\n + это следующий канал" +
+                "\n - это предыдущий канал");
+        }
+        public int TVChannelNext()
+        {
+            if (correctChannel < 10)
+            {
+                correctChannel++;
+                TVChannel = correctChannel;
+                return correctChannel;
+            }
+            else
+            {
+                correctChannel = 0;
+                TVChannel = correctChannel;
+                return correctChannel;
+            }
+        }
+        int TVChannelPrevious()
+        {
+            if (correctChannel > 0)
+            {
+                correctChannel--;
+                TVChannel = correctChannel;
+                return correctChannel;
+            }
+            else
+            {
+                correctChannel = 10;
+                TVChannel = correctChannel;
+                return correctChannel;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                TV TV1 = new TV();
+                    TV1.TVOn();
+                string Enter = Console.ReadLine();
+                while (TV1.TVEnabled == true)
+                {
+                   if (Enter == "+") 
+                    {
+                        Console.WriteLine("Канал  " + TV1.TVChannelNext());
+                    }
+                    else if (Enter == "-")
+                    {
+                        Console.WriteLine("Канал  " + TV1.TVChannelPrevious());
+                    }
+                    Console.Write("Какой будет следующий канал? ");
+                    Enter = Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
